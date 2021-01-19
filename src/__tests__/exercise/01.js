b67d58365402f6048cfc5c41e696f15b467cc130
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2021-01-19 18:26:11 +1100

    installed and began test challenges

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -7,16 +7,23 @@ import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
+  const div = document.createElement("div")
   //
   // 🐨 append the div to document.body (💰 document.body.append)
+  document.body.append(div)
   //
   // 🐨 use ReactDOM.render to render the <Counter /> to the div
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+
+  ReactDOM.render(<Counter />, div)
+
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
   //
+  const message = div.firstChild.querySelector('div')
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  // expect(message.textContent.toBe('Current count: 0'))
   // 🐨 click the increment button (💰 increment.click())
   // 🐨 assert the message.textContent
   // 🐨 click the decrement button (💰 decrement.click())
