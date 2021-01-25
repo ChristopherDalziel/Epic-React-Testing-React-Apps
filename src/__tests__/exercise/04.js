bc609f0d5c84f160c2ed45f284fd63a5538317bd
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2021-01-25 17:07:05 +1100

    extra credit 1 complete

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -2,26 +2,32 @@
 // http://localhost:3000/login
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
+  // let submittedData = null
   // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+
+  // ** EXTRA CREDIT 1 ** - Use a jest mock function (This is better than creating a local variable)
+  const handleSubmit = jest.fn()
+  const username = 'Chris'
+  const password = 'password'
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', { name: /submit/i }))
+
+
+  // expect(submittedData).toEqual({ password: 'password', username: 'chris' })
+
+  // ** EXTRA CREDIT 1 ** - Use mock functions / toHaveBeenCalledWith
+  expect(handleSubmit).toHaveBeenCalledWith({ username, password })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalled()
 })
 
 /*
