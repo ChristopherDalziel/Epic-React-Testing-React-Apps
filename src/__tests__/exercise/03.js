2f6e65ccfbc7333aa88824ab7fda43292e8caefa
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2021-01-25 16:32:20 +1100

    03 completed + extra credit & notes

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,20 +2,22 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  // ** STEP ONE ** - Improve our tests so they're unaware of the order the buttons are presented on the page etc, or where the text content is coming from. This is a massive improvement because our tests will still function if we make changes to the code base, this is also why we use rajex to ignore case. If we were to change the container of our 'current count' now from a div to a <p> for example.. our test would still be able to find this information and pass regardless. Or if our buttons switched order etc.
+  render(<Counter />)
+  const increment = screen.getByRole('button', { name: /increment/i })
+  const decrement = screen.getByRole('button', { name: /decrement/i })
+  const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
+  // Normal fireEvent (Not recommended while userEvent is usable)
   fireEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  // ** EXTRA CREDIT ** - imported userEvent. As a user, you can activate this event in many ways. userEvent is going to activate our event in serval different ways such as mouseDown, mouseUp etc. This is far better than using just 'fireEvent' for implimentation details.
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
