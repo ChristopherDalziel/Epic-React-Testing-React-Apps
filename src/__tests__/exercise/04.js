f1c77b12bde7609f387f9bef07f8322b1867ff6e
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2021-01-26 21:25:57 +1100

    extra credit 3 complete

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -8,13 +8,14 @@ import Login from '../../components/login'
 import faker from 'faker'
 
 // ** EXTRA CREDIT 2 ** - Setting a random username/password is a good idea because if another user comes and looks at your test implimentation they may think that 'Chris' and 'password' are important values. By randomising your results you make clear that your form should take in any data.
-function buildLoginForm() {
+function buildLoginForm(overrides) {
   return {
     username: faker.internet.userName(),
     password: faker.internet.password(),
+    // ** EXTRA CREDIT 3 ** - Allow for overrides 
+    ...overrides
   }
 }
-
 
 test('submitting the form calls onSubmit with username and password', () => {
   // let submittedData = null
@@ -26,7 +27,8 @@ test('submitting the form calls onSubmit with username and password', () => {
   // const password = 'password'
 
   render(<Login onSubmit={handleSubmit} />)
-  const { username, password } = buildLoginForm()
+  // ** EXTRA CREDIT 3 ** - Override password - The benefit of this is the password may have some form of validation on it to be a certain strength etc for this reason we may not want to use something randomly generated
+  const { username, password } = buildLoginForm({ password: 'dog' })
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
