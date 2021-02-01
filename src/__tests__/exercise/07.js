bfe63e82bb39463614412969fae10eb825b014b8
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2021-02-01 17:06:17 +1100

    07 complete

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,21 +2,55 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+// import { render, screen } from '@testing-library/react'
+import { render, screen } from 'test/test-utils'
+// import { ThemeProvider } from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+// ** EXTRA CREDIT TWO ** - Stop duplicating test data by creating a custom render method that encapsulated the shared logic
+// function renderWithInfo(ui, { theme = 'light', ...options } = {}) {
+//   const Wrapper = ({ children }) => (
+//     <ThemeProvider value={[theme, () => { }]}>{children}</ThemeProvider>
+//   )
+//   return render(ui, { wrapper: Wrapper, ...options })
+// }
+
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  // ** EXTRA CREID THREE ** 
+  render(<EasyButton>Easy</EasyButton>)
+
+  // ** EXTRA CREDIT TWO ** 
+  // renderWithInfo(<EasyButton>Easy</EasyButton>)
+
+  const button = screen.getByRole('button', { name: /easy/i })
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+})
+
+// ** EXTRA CREDIT ONE **
+test('renders with the dark styles for the dark theme', () => {
+  // ** EXTRA CREID THREE ** 
+  render(<EasyButton>Easy</EasyButton>, {
+    theme: 'dark'
+  })
+
+  // ** EXTRA CREDIT TWO ** 
+  // renderWithInfo(<EasyButton>Easy</EasyButton>, {
+  //   theme: 'dark'
+  // })
+
+  // function Wrapper({ children }) {
+  //   return <ThemeProvider initialTheme="dark" >{children}</ThemeProvider>
+  // }
+
+  // render(<EasyButton>Easy</EasyButton>, { wrapper: Wrapper })
+  const button = screen.getByRole('button', { name: /easy/i })
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
